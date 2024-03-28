7ad2fe67f02648c45fb80fc84f3bfb2fb30ee797
Ionos Cloud SDK Robot <sdk@cloud.ionos.com>
2024-03-28 10:56:17 +0000

    auto-generated version 1.0.1

--- configuration.ts
+++ configuration.ts
@@ -120,7 +120,7 @@ export class Configuration {
         this.maxRetries = DEFAULT_MAX_RETRIES;
         this.maxWaitTime = DEFAULT_MAX_WAIT_TIME;
         this.debug = false;
-        this.setUserAgent('ionos-cloud-sdk-node-dbaas-mariadb/v1.0.0');
+        this.setUserAgent('ionos-cloud-sdk-node-dbaas-mariadb/v1.0.1');
     }
 
     static fromEnv(): Configuration {
